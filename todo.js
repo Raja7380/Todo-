d14a2d7f61f666c4b4a82719acc9f5a707c64111
Raja7380 <126516371+Raja7380@users.noreply.github.com>
2024-01-24 21:21:44 +0530

    Update todo.js

--- todo.js
+++ todo.js
@@ -13,13 +13,21 @@ var c=0;
 
   app.post('/todos', (req, res) => {
     const newTodo = {
-    //   id: Math.floor(Math.random() * 1000000), // unique random id
       id:c,
       title: req.body.title,
       description: req.body.description,
     };
     c=c+1;
     todos.push(newTodo);
-    //res.status(201).json(newTodo);
+    res.status(201).json(newTodo);
+  });
+app.delete('/todos/:id', (req, res) => {
+    const todoIndex = todos.findIndex(t => t.id === parseInt(req.params.id));
+    if (todoIndex === -1) {
+      res.status(404).send();
+    } else {
+      todos.splice(todoIndex, 1);
+      res.status(200).send();
+    }
   });
 app.listen(3000)
